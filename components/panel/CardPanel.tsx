999c038980c510d87ce1dc5fd6af279fadc60111
sharshakap <55374102+sharshakap@users.noreply.github.com>
2021-11-14 21:26:04 -0300

    fix: detalles responsive cards

--- components/panel/CardPanel.tsx
+++ components/panel/CardPanel.tsx
@@ -9,24 +9,29 @@ import Link from "next/link";
  * **/
 const CardPanel = ({ title, children, href }: any) => {
   return (
-    <div className="col-lg-4 cardPanel">
-      <div className="card text-center border rounded-3 border-primary shadow-1 m-3">
-        <div className="card-header p-0">
-          <Message showIcon type="info">
-            {title}
-          </Message>
-        </div>
-        <div className="card-body">
-          <p className="card-text">{children}</p>
-          <br />
-          <Link href={href} passHref>
-            <button type="button" className="btn btn-primary cardPanel__irBtn">
-              Ir
-            </button>
-          </Link>
+    <>
+      <div className="col-lg-4 cardPanel">
+        <div className="card text-center border rounded-3 border-primary shadow-1 m-3">
+          <div className="card-header p-0">
+            <Message showIcon type="info">
+              {title}
+            </Message>
+          </div>
+          <div className="card-body">
+            <p className="card-text">{children}</p>
+            <br />
+            <Link href={href} passHref>
+              <button
+                type="button"
+                className="btn btn-primary cardPanel__irBtn"
+              >
+                Ir
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
